7ab97426c31285e44e2edb8c5c6206bfd3ce764d
[dyad] <git@dyad.sh>
2025-11-04 20:51:28 -0300

    [dyad] Fixed import path for ToastProvider in index.tsx - wrote 2 file(s)

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ToastProvider from './components/ToastProvider';
+import ToastProvider from './src/components/ToastProvider';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
